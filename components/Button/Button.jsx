e66a0d2f99b95490d7d982a39f976938cf38844f
Alexey <youtwigrif@gmail.com>
2024-05-29 23:09:13 -0700

    change

--- components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = () => {
-    return (
-        <button {...props} className={'button ' + props.className}/>
-    )
-}
-
-export default Button
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useTelegram } from "../../src/hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
+import Button from "../Button/Button";
 
 const Header = () => {
     const {tg, user, onClose} = useTelegram()
