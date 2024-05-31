10823cc92ebc87aa80a4ec0f67068db3f52ece2d
Alexey <youtwigrif@gmail.com>
2024-05-30 18:02:33 -0700

    b

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,16 +7,12 @@ import "../Form/Form.css"
 const Form = () => {
     const [country, setCountry] = useState()
     const [street, setStreet] = useState()
-    const [state, setState] = useState()
-    const [zip, setZip] = useState()
-    const [subject, setSubject] = useState('physical')
     const {tg} = useTelegram()
 
     const onSendData = useCallback(() => {
         const data ={ 
             country,
             street,
-            subject
         }
         tg.sendData(JSON.stringify(data))
     })
@@ -50,31 +46,12 @@ const Form = () => {
     const onChangeStreet = (e) => {
         setStreet(e.target.value)
     }
-
-    const onChangeState = (e) => {
-        setState(e.target.value)
-    }
-    
-    const onChangeZip = (e) => {
-        setZip(e.target.value)
-    }
-
-    const onChangeSubject = (e) => {
-        setSubject(e.target.value)
-    }
  
     return (
         <div className="Form">
             <h3>Type your data</h3>
             <input type="text" value={country} onChange={onChangeCountry} placeholder={'Country'} />
             <input type="text" value={street} onChange={onChangeStreet} placeholder={'Street'} />
-            <input type="text" value={state} onChange={onChangeStreet} placeholder={'State'} />
-            <input type="text" value={zip} onChange={onChangeZip} placeholder={'Zip'} />
-
-            <select value={subject} onChange={onChangeSubject} className={'select'}>
-                <option value={'physical'}>Phiz.lico</option>
-                <option value={'legal'}>Your.lico</option>
-            </select>
         </div>
     )
 }
